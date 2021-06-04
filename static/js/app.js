e491fa769655c41999728b6f89201b30e1805e73
fkimani <mainafk@gmail.com>
2021-06-03 21:04:58 -0400

    edits to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 // Use d3 to update data table with week and gas price
-d3.json("../../../resources/data_sample.json").then((data) => {
+d3.json("http://localhost:5000/getdata").then((data) => {
     // log data 
     console.log(data);
 
