d5d3648662a7a59e2e0f5a70995dc024566f928a
fkimani <mainafk@gmail.com>
2021-06-09 19:07:33 -0400

    edits app js

--- static/js/app.js
+++ static/js/app.js
@@ -8,44 +8,56 @@ d3.json("http://localhost:5000/getdata").then(function (data) {
     const parseTime = d3.timeParse('%b %d, %Y');
 
 
-    //d3.nest 
+    //d3.nest to group our data by year (groupedData is an array of objects)
     var groupedData = d3.nest()
         .key(function (dt) { return formatYear(parseTime(dt.date)); })
-        .key(function (dt) { return formatMonth(parseTime(dt.date)); })
-        .key(function (dt) { return dt.price; })
         .entries(data.history);
 
+    console.log(groupedData);
 
     groupedData.forEach(function (d) {
-        // console.log(d);
-        // stepping into the groupedData for prices by month
-        for (var i = 0; i < d.values.length - 1; i++) {
-            for (var j = 0; j < i; j++) {
-                console.log(d.values[i].key)
-               
-            }
-        }
+        // console.log(d.values);
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
+        // for (var i = 0; i < d.values.length; i++) {
+        //     //    console.log(d.values[i]);
+        // }
+
+
+        // use d3 to populate data table
         d3.select("tbody")
             .selectAll("tr")
             .data(groupedData)
             .enter()
             .append("tr")
             .html(function (d) {
-                return `<td>${d.key}</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>`;
+                // console.log(d.values);
+                // date/price map
+                // d.values.forEach(function(dv){
+                //     // console.log(dv);
+                // })
+
+                return `<td>${d.key}</td>
+                <td></td ><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>`;
             });
 
     });
 
     //function to get price ave for each month
     function monthAve(array) {
+        // console.log(array);
+
         var i = 0, sum = 0, len = array.length;
+        for (var i = 0; i < len; i++) {
+            for (var j = 0; j < i; j++) {
+                console.log(array[j]);
+            }
+        }
         while (i < len) {
             sum = sum + array[i++];
         }
         return sum / len;
     }
-
 
 
 });
