2f3aed1cd896dae621e0bd3abeed8a386e51ddb7
Jay Cha <kimchikimchi@gmail.com>
2021-06-09 20:01:20 -0400

    - Deleted unused JS files. - Adding monthly avg price parsing and calculation

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,17 @@
+const calculateMonthlyAvg = function (array) {
+    let avg_price = 0;
+    let total_price = 0;
+    array.forEach(obj => { 
+        
+        obj.price = +obj.price;
+        total_price += obj.price;
+    })
+
+    avg_price = total_price / array.length;
+    return avg_price;
+};
+
+
 // Use d3 to update data table with week and gas price
 d3.json("http://localhost:5000/getdata").then(function (data) {
     // console.log(data.history);
@@ -8,12 +22,50 @@ d3.json("http://localhost:5000/getdata").then(function (data) {
     const parseTime = d3.timeParse('%b %d, %Y');
 
 
-    //d3.nest 
+    /*
+        This should return nested object of 
+        <YYYY>: { <MMMM>: [<obj1>, <obj2>, ... ]}
+        ...
+        See https://github.com/d3/d3-collection for details
+    */
     var groupedData = d3.nest()
         .key(function (dt) { return formatYear(parseTime(dt.date)); })
         .key(function (dt) { return formatMonth(parseTime(dt.date)); })
-        .key(function (dt) { return dt.price; })
+        //.key(function (dt) { return dt.price; })
         .entries(data.history);
+
+    // console.log(groupedData);
+    
+    // Should old monthly price totals
+    let monthlyPrices = {};
+
+    groupedData.forEach(yearlyData => {
+        let year = yearlyData.key;
+        // console.log(`Starting ${year}`);
+        monthlyPrices[year] = {};
+
+        yearlyData.values.forEach(monthlyData => {
+   
+            const month =  monthlyData.key;
+            // console.log(`Starting ${month}`);
+            const monthly_price = calculateMonthlyAvg(monthlyData.values);
+            // console.log(monthly_price);
+    
+            // console.log(`avg gas price for ${year} and ${month} is ${monthly_price}`);
+
+            // Storing in the following fashion
+            // result = {
+            //     1993 : {
+            //         'jan': price,
+            //         'feb': price
+            //     }
+            //  }...
+            
+            monthlyPrices[year][month] = monthly_price;
+        });
+    });
+
+    console.log(monthlyPrices);
 
 
     groupedData.forEach(function (d) {
@@ -21,7 +73,7 @@ d3.json("http://localhost:5000/getdata").then(function (data) {
         // stepping into the groupedData for prices by month
         for (var i = 0; i < d.values.length - 1; i++) {
             for (var j = 0; j < i; j++) {
-                console.log(d.values[i].key)
+                //console.log(d.values[i].key)
                
             }
         }
