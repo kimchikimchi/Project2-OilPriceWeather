ac52404e21f75fb7e8db83e8370a50d042f933df
Jay Cha <kimchikimchi@gmail.com>
2021-06-09 23:36:26 -0400

    - Fixed table content rendering using data binding

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,15 @@
+// Helper function to calculate average of objects passed in an array
 const calculateMonthlyAvg = function (array) {
     let avg_price = 0;
     let total_price = 0;
     array.forEach(obj => { 
-        
+        // Convert price from string to a number.
         obj.price = +obj.price;
         total_price += obj.price;
     })
 
     avg_price = total_price / array.length;
-    return avg_price;
+    return avg_price.toFixed(3);
 };
 
 
@@ -42,15 +43,12 @@ d3.json("http://localhost:5000/getdata").then(function (data) {
     groupedData.forEach(yearlyData => {
         let year = yearlyData.key;
         // console.log(`Starting ${year}`);
+        // Initialize year object
         monthlyPrices[year] = {};
 
         yearlyData.values.forEach(monthlyData => {
-   
             const month =  monthlyData.key;
-            // console.log(`Starting ${month}`);
             const monthly_price = calculateMonthlyAvg(monthlyData.values);
-            // console.log(monthly_price);
-    
             // console.log(`avg gas price for ${year} and ${month} is ${monthly_price}`);
 
             // Storing in the following fashion
@@ -60,44 +58,27 @@ d3.json("http://localhost:5000/getdata").then(function (data) {
             //         'feb': price
             //     }
             //  }...
-            
             monthlyPrices[year][month] = monthly_price;
         });
     });
 
-    console.log(monthlyPrices);
+    // console.log(monthlyPrices);
 
+    const tbody = d3.select("tbody");
+    let tr = tbody;
 
-    groupedData.forEach(function (d) {
-        // console.log(d);
-        // stepping into the groupedData for prices by month
-        for (var i = 0; i < d.values.length - 1; i++) {
-            for (var j = 0; j < i; j++) {
-                //console.log(d.values[i].key)
-               
-            }
-        }
-
-        d3.select("tbody")
-            .selectAll("tr")
-            .data(groupedData)
-            .enter()
-            .append("tr")
-            .html(function (d) {
-                return `<td>${d.key}</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>`;
-            });
-
-    });
-
-    //function to get price ave for each month
-    function monthAve(array) {
-        var i = 0, sum = 0, len = array.length;
-        while (i < len) {
-            sum = sum + array[i++];
-        }
-        return sum / len;
-    }
-
-
+    const monthlyPricesArr = Object.entries(monthlyPrices);
+    // Data binding and HTML table rendering. Wish the td rendering was more elegant but it works.
+    d3.select("tbody")
+        .selectAll("tr")
+        .data(monthlyPricesArr)
+        .enter()
+        .append("tr")
+        .html(function(d) {
+                return  `<td><h7><b>${d[0]}<b></h7></td> <td>${d[1].Jan||'N/A'}</td> <td>${d[1].Feb||'N/A'}</td> <td>${d[1].Mar||'N/A'}`
+                        + `</td> <td>${d[1].Apr||'N/A'}</td> <td>${d[1].May||'N/A'}</td> <td>${d[1].Jun||'N/A'}</td> `
+                        + `<td>${d[1].Jul||'N/A'}</td> <td>${d[1].Aug||'N/A'}</td> <td>${d[1].Sep||'N/A'}</td> `
+                        + `<td>${d[1].Oct||'N/A'}</td> <td>${d[1].Nov||'N/A'}</td> <td>${d[1].Dec||'N/A'}</td>`;
+        });
 
 });
